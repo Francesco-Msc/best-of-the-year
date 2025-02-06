8785871bfb0bb6f792f5a66fdea9d91d2cb81671
Francesco-Msc <moscafrancesco1998@gmail.com>
2025-02-06 20:58:42 +0100

    added getBestMovies and getBestSongs methods to BestController returning lists of movies and songs

--- src/main/java/org/lessons/springboot/bestoftheyear/best_of_the_year/controller/PageController.java
+++ src/main/java/org/lessons/springboot/bestoftheyear/best_of_the_year/controller/PageController.java
@@ -1,11 +1,15 @@
 package org.lessons.springboot.bestoftheyear.best_of_the_year.controller;
 
+import java.util.Arrays;
+import java.util.List;
+
+import org.lessons.springboot.bestoftheyear.best_of_the_year.models.Movies;
+import org.lessons.springboot.bestoftheyear.best_of_the_year.models.Songs;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
 @Controller
 @RequestMapping("/")
 public class PageController {
@@ -13,7 +17,16 @@ public class PageController {
     @GetMapping("/")
     public String index(@RequestParam String name, Model model){
         model.addAttribute("name", name);
+        model.addAttribute("movies", getBestMovies());
+        model.addAttribute("songs", getBestSongs());
         return "index";
     }
-    
+
+    private List<Movies> getBestMovies(){
+        return Arrays.asList(new Movies(1, "Interstellar"));
+    }
+
+    private List<Songs> getBestSongs(){
+        return Arrays.asList(new Songs(1, "Never gonna give you up"));
+    }
 }
