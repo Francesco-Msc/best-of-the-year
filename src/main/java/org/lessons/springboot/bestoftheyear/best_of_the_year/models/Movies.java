c1bb64bf88bac664f16f795959f9adf97cc9b3bb
Francesco-Msc <moscafrancesco1998@gmail.com>
2025-02-07 19:53:19 +0100

    added genere and duration fields in Movies class

--- src/main/java/org/lessons/springboot/bestoftheyear/best_of_the_year/models/Movies.java
+++ src/main/java/org/lessons/springboot/bestoftheyear/best_of_the_year/models/Movies.java
@@ -3,25 +3,38 @@ package org.lessons.springboot.bestoftheyear.best_of_the_year.models;
 public class Movies {
     private int id;
     private String title;
+    private String genre;
+    private String duration;
 
-    public Movies(int id, String title){
+    public Movies(int id, String title, String genre, String duration){
         this.id = id;
         this.title = title;
+        this.genre = genre;
+        this.duration = duration;
     }
 
     public int getId(){
         return id;
     }
-
     public void setId(int id) {
         this.id = id;
     }
-
     public String getTitle() {
         return title;
     }
-    
     public void setTitle(String title) {
         this.title = title;
+    }
+    public String getGenre() {
+        return genre;
+    }
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+    public String getDuration() {
+        return duration;
+    }
+    public void setDuration(String duration) {
+        this.duration = duration;
     }
 }
