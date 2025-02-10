f88bebe28ab6f8db95c90ac7326f0cd7d547b69f
Francesco-Msc <moscafrancesco1998@gmail.com>
2025-02-10 16:38:34 +0100

    added new movies and songs to lists

--- src/main/java/org/lessons/springboot/bestoftheyear/best_of_the_year/service/Details.java
+++ src/main/java/org/lessons/springboot/bestoftheyear/best_of_the_year/service/Details.java
@@ -14,7 +14,8 @@ public class Details {
         return Arrays.asList(
             new Songs(1, "Creep", "Radiohead", "XFkzRNyygfk"),
             new Songs(2, "Numb", "Linkin Park", "kXYiU_JCYtU"),
-            new Songs(3, "21 Guns", "Green Day", "r00ikilDxW4")
+            new Songs(3, "21 Guns", "Green Day", "r00ikilDxW4"),
+            new Songs(4, "Now I Know Their Name", "Paolo Mosca", "0a9BqkhtPo0")
             );
     }
 
@@ -22,7 +23,10 @@ public class Details {
         return Arrays.asList(
             new Movies(1, "Interstellar", "Sci-fi/Avventura", "2h 49m"),
             new Movies(2, "Dune", "Sci-fi/Avventura", "2h 35m"),
-            new Movies(3, "Spider-Man: No Way Home", "Azione/Sci-fi", "2h 28m")
+            new Movies(3, "Spider-Man: No Way Home", "Azione/Sci-fi", "2h 28m"),
+            new Movies(4, "Il Signore degli Anelli - La Compagnia dell'Anello", "Fantastico/Avventura", "2h 58m"),
+            new Movies(5, "Matrix", "Azione/Sci-fi", "2h 16m"),
+            new Movies(6, "Una notte da leoni", "Commedia/Avventura", "1h 40m")
             );
     }
 
